69f0ce217e814473204b574d7aaf2a7099e321be
Bruno Marini <bruno.marini1@gmail.com>
2020-09-17 18:32:27 -0300

    [+] Index: new factory and start to mine

---
+++ index.js
@@ -0,0 +1,15 @@
+// -- Requires --
+require('dotenv').config();
+const Factory = require('./src/factory');
+
+// -- Configuration --
+// GitHub API token
+const token = process.env.TOKEN;
+// Quantity of pages as objective (5 at time)
+const pages = 1;
+
+// New factory to mine
+const factory = new Factory(token, pages);
+
+// Start to mine
+factory.start();
