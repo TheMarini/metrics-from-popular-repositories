f0fdcb8ccc52353c31cde5a61c2ae585236af3a5
Bruno Marini <bruno.marini1@gmail.com>
2020-09-17 20:19:09 -0300

    [^] Mine: polish

--- src/mine.js
+++ src/mine.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const fetch = require('./utils/fetch');
 const l = require('./utils/logger');
 
@@ -6,7 +7,6 @@ class Mine {
     this.objective = objective;
     this.current = current;
     this.cursor = null;
-    this.storage = [];
   }
 
   async start(token) {
@@ -25,8 +25,10 @@ class Mine {
     try {
       await fetch(token, this.cursor).then((res) => {
         this.cursor = res.pageInfo.endCursor || null;
-        l.info('Página encontrada. Formatando...\n');
-        this.storage.push(Mine.polish(res.nodes));
+        l.info('Página encontrada. Formatando... ');
+        const data = Mine.polish(res.nodes);
+        l.info('Salvando...\n');
+        Mine.store(data);
         this.current += 1;
       });
     } catch (e) {
@@ -35,18 +37,30 @@ class Mine {
     }
   }
 
+  // eslint-disable-next-line no-unused-vars
+  static store(data) {
+    // console.log(data);
+  }
+
   static polish(dirt) {
     return dirt.map((repo) => {
       return {
         '<usuário>/<repositório>': repo.nameWithOwner,
-        'Data de criação': repo.createdAt,
-        'Data do último push': repo.pushedAt,
         'Nº de estrelas': repo.stargazers.totalCount,
-        'Nº de PRs aceitas': repo.mergedPullRequests.totalCount,
-        'Nº de releases': repo.releases.totalCount,
+        'Idade (anos)': moment().diff(repo.createdAt, 'years', true).toFixed(2),
+        'Data de criação': repo.createdAt,
+        'Último push (dias)': moment()
+          .diff(repo.pushedAt, 'days', true)
+          .toFixed(2),
+        'Data do último push': repo.pushedAt,
         'Linguagem principal': repo.primaryLanguage
           ? repo.primaryLanguage.name
           : 'N/A',
+        'Nº de PRs aceitas': repo.mergedPullRequests.totalCount,
+        'Nº de releases': repo.releases.totalCount,
+        'Proporção de issues fechadas': (
+          repo.closedIssues.totalCount / repo.totalIssues.totalCount || 0
+        ).toFixed(2),
         'Issues fechadas': repo.closedIssues.totalCount,
         'Total de issues': repo.totalIssues.totalCount,
       };
