69072245c82a9d808141ae4a96fc21a809bfbcd2
Bruno Marini <bruno.marini1@gmail.com>
2020-09-17 13:34:49 -0300

    [+] Logger: states and jump

---
+++ src/utils/logger.js
@@ -0,0 +1,14 @@
+module.exports = {
+  info(m) {
+    console.log(m);
+  },
+  success(m) {
+    console.log(m);
+  },
+  error(m) {
+    console.error(m);
+  },
+  jump() {
+    console.log('');
+  },
+};
