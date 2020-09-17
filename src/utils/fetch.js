f22651c5139ed84d19e2281142c3be573c10076e
Bruno Marini <bruno.marini1@gmail.com>
2020-09-17 12:51:52 -0300

    [+] Fetch: a fetch for GitHub API

---
+++ src/utils/fetch.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const query = require('./query');
+
+module.exports = (
+  token,
+  cursor = null,
+  urlAPI = 'https://api.github.com/graphql'
+) => {
+  return axios
+    .post(
+      urlAPI,
+      {
+        query: query(cursor),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.data.search;
+    })
+    .catch((error) => {
+      console.error('Erro na requisição: ', error);
+    });
+};
